4eadeeab1d792b5e3523e7f8f650c61938aee11d
Douglas Paz <douglaspaz955@gmail.com>
2025-11-07 12:46:24 -0300

    feat(products): adiciona endpoint DELETE para remover produtos

--- src/product.service.ts
+++ src/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './product.entity';
+import { DeleteResult } from 'typeorm';
 
 @Injectable()
 export class ProductService {
@@ -17,5 +18,11 @@ export class ProductService {
 
   async createProduct(product: Product): Promise<Product> {
     return this.productRepository.save(product);
+  }
+
+  async deleteProduct(id: number): Promise<number> {
+    const result: DeleteResult = await this.productRepository.delete(id);
+    
+    return result.affected || 0; 
   }
 }
